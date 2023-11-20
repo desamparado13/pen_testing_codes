e3c6c4ca05f6c23ad7f77c0e52d63507e5bdf9cf
desamparado13 <143670379+desamparado13@users.noreply.github.com>
2023-11-20 19:46:49 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,3 +14,11 @@ echo "[+] Harvesting Subdomains with Assetfinder...."
 assetfinder $url >> $url/recon/assets.txt
 cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
 rm $url/recon/assets.txt
+
+#echo "[+] Harvesting Subdomains with Amass..."
+#amass enum -d $url >> $url/recon/f.txt
+#sort -u $url/recon/f.txt >> $url/recon/final.txt
+#rm $url/recon/f.txt
+
+echo "[+] Probing for alive domains..."
+cat $url | sort -u | httprobe -s -p https:443 sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/alive.txt
