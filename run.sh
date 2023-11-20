6d9c745e1e04d501972e6e404784864df498a17e
desamparado13 <143670379+desamparado13@users.noreply.github.com>
2023-11-20 19:01:41 +0800

    Create run.sh
    assetfinder code to find subdomains

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+url=$1
+
+if [ ! -d "$url" ];then
+        mkdir $url
+fi
+
+if [ ! -d "$url/recon" ];then
+        mkdir $url/recon
+fi
+
+echo "[+] Harvesting Subdomains with Assetfinder...."
+assetfinder $url >> $url/recon/assets.txt
+cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
+rm $url/recon/assets.txt
